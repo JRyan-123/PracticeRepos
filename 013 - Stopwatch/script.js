7437214b565412382fcc3694143e0977165cf514
JRyan-123 <acostajoseryan4@gmail.com>
2025-06-27 00:32:37 +0800

    add js for timer

---
+++ 013 - Stopwatch/script.js
@@ -0,0 +1,40 @@
+let [seconds , minutes , hours] = [0 , 0 , 0];
+let displayTime	= document.getElementById('display');
+let timer = null;
+function stopwatch(){
+	seconds++;
+	if (seconds == 60) {
+		seconds = 0;
+		minutes++
+		if (minutes == 60) {
+			minutes = 0;
+			hours++
+		}
+	}
+	let h = hours < 10 ? "0" + hours : "0";
+	let m = minutes < 10 ? "0" + minutes : "0";
+	let s = seconds < 10 ? "0" + seconds : "0";
+
+	displayTime.innerHTML = h + " : " + m + " : " + s;
+}
+
+function start() {
+	if (timer!== null) {
+		clearInterval(timer);
+	}
+	timer = setInterval(stopwatch, 1000);
+	displayTime.style.color = "#0ff3ff";
+}
+function stop() {
+	clearInterval(timer);
+	displayTime.style.color = "#ff1212";
+	if (timer === null) {
+		displayTime.style.color = "white";
+	}
+}
+function reset() {
+	clearInterval(timer);
+	[seconds , minutes , hours] = [0 , 0 , 0];
+	displayTime.innerHTML = "00 : 00 : 00";
+	displayTime.style.color = "white";
+}
