844f163b31bbc2ac5b163f7dbbf5ad01905fb9ec
JRyan-123 <acostajoseryan4@gmail.com>
2025-07-02 16:22:29 +0800

    >add span logo for resize

--- 016 - BgChangeFX/script.js
+++ 016 - BgChangeFX/script.js
@@ -1,6 +1,8 @@
 const imgBox = document.querySelector('.img-box');
 const imgWrap = document.querySelector('.img-wrap');
 const fullImg = document.getElementById('full-img');
+const line = document.getElementById('line');
+
 
 fullImg.style.width = imgBox.offsetWidth + "px";
 
@@ -9,4 +11,5 @@ let leftSpace = imgBox.offsetLeft;
 imgBox.addEventListener("mousemove", (e) => {
 	let boxWidth = (e.pageX - leftSpace) + "px";
 	imgWrap.style.width = boxWidth;
+	line.style.left = boxWidth;
 });
