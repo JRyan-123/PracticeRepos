5ed8f0934b3035b0f3bf69cab95403558a427d74
JRyan-123 <acostajoseryan4@gmail.com>
2025-07-03 20:53:49 +0800

    add voices and text to voice js

---
+++ 019 - TextToVoiceConverter/script.js
@@ -0,0 +1,26 @@
+const btn = document.getElementById('btn');
+const txtInput = document.getElementById('textInput');
+let speech = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+let voiceSelect = document.querySelector('select');
+
+window.speechSynthesis.onvoiceschanged = () => {
+	voices = window.speechSynthesis.getVoices();
+	speech.voice = voices[0];
+
+	voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+};
+voiceSelect.addEventListener("change", () => {
+	speech.voice = voices[voiceSelect.value];
+
+});
+
+btn.addEventListener("click", () => {
+	speechSynthesis.cancel(); //stops ongoing and start new
+	speech.text = txtInput.value;
+	window.speechSynthesis.speak(speech);
+	speech.voice = voices[0];
+
+});
